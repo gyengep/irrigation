905aaf6bb824ec62ad6c26176eae2d07e24fd659
Peter <gyengep@gmail.com>
2016-08-03 10:23:30 +0200

    pi forditashoz szukseges libraryk

--- Irrigation/objects.mk
+++ Irrigation/objects.mk
@@ -2,7 +2,7 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-USER_OBJS :=
+USER_OBJS := 
 
-LIBS :=
+LIBS := -lpthread -lwiringPi
 
