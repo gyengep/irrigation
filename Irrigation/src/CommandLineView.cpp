e6572482a9462f9f10012dc05d6f0bd00663d6ea
Peter Gyenge <gyengep@gmail.com>
2016-09-22 11:04:55 +0200

    CommandLineView pelda

--- Irrigation/src/CommandLineView.cpp
+++ Irrigation/src/CommandLineView.cpp
@@ -138,6 +138,31 @@ void CommandLineView::workerFunc() {
 		tokenize(line, tokens);
 
 /*
+int main(void) {
+    fd_set rfds;
+    struct timeval tv;
+    int retval;
+
+    // Watch stdin (fd 0) to see when it has input.
+    FD_ZERO(&rfds);
+    FD_SET(0, &rfds);
+    // Wait up to five seconds.
+    tv.tv_sec = 5;
+    tv.tv_usec = 0;
+    retval = select(1, &rfds, NULL, NULL, &tv);
+    // Don’t rely on the value of tv now!
+
+    if (retval == -1)
+        perror("select()");
+    else if (retval)
+        printf("Data is available now.\n");
+        // FD_ISSET(0, &rfds) will be true.
+    else
+        printf("No data within five seconds.\n");
+    return 0;
+} */
+
+/*
 		std::cout << "Tokens:";
 		for (unsigned i = 0; i < tokens.size(); ++i) {
 			std::cout << " \"" << tokens[i] << "\"";
