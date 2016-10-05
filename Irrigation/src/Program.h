9623720c29c20090ff5bc3de0be383c0e4d6065d
Peter Gyenge <gyengep@gmail.com>
2016-10-05 16:14:39 +0200

    Program copy ctor es operator= letiltva

--- Irrigation/src/Program.h
+++ Irrigation/src/Program.h
@@ -19,6 +19,9 @@ class Scheduler;
 class SpecifiedScheduler;
 
 class Program {
+	Program(const Program&);
+	void operator= (const Program&);
+
 public:
 
 	typedef RunTimeContainer::RunTimes RunTimes;
