7f282c6d3a49ca5faaa9637e74141d19d6ee73c2
gyengep <gyengep@gmail.com>
2018-07-02 23:40:21 +0100

    irrigation_service init script

--- Irrigation/src/irrigation.cpp
+++ Irrigation/src/irrigation.cpp
@@ -1,8 +1,5 @@
-#include <iostream>
 #include <signal.h>
 #include <stdexcept>
-#include <sys/stat.h>
-#include <unistd.h>
 #include "Logger/Logger.h"
 #include "Model/IrrigationApplication.h"
 #include "Model/Configuration.h"
@@ -15,68 +12,15 @@ void signal_handler(int signo) {
 	Application::getInstance().terminate();
 }
 
-bool initLogger() {
-	try {
-		LOGGER.setLevel(Configuration::getInstance().getLogLevel());
-		LOGGER.setFileName(Configuration::getInstance().getLogFileName());
-	} catch (const exception& e) {
-		cerr << "Exception: " << e.what() << endl;
-		return false;
-	}
-
-	return true;
-}
-
-void daemonize() {
-	/* Our process ID and Session ID */
-	pid_t pid, sid;
-
-	/* Fork off the parent process */
-	pid = fork();
-	if (pid < 0) {
-		exit(EXIT_FAILURE);
-	}
-
-	/* If we got a good PID, then we can exit the parent process. */
-	if (pid > 0) {
-		exit(EXIT_SUCCESS);
-	}
-
-	/* Change the file mode mask */
-	umask(0);
-
-	/* Open any logs here */
-	if (!initLogger()) {
-		exit(EXIT_FAILURE);
-	}
-
-	/* Create a new SID for the child process */
-	sid = setsid();
-	if (sid < 0) {
-		LOGGER.error("Can not create SID");
-		exit(EXIT_FAILURE);
-	}
-
-	/* Change the current working directory */
-	if ((chdir("/")) < 0) {
-		LOGGER.error("Can not change working directory");
-		exit(EXIT_FAILURE);
-	}
-
-	/* Close out the standard file descriptors */
-	close(STDIN_FILENO);
-	close(STDOUT_FILENO);
-	close(STDERR_FILENO);
-}
-
 int main() {
-
-	daemonize();
 
 	try {
 		if (signal(SIGTERM, signal_handler) == SIG_ERR) {
 			throw runtime_error("Can't catch SIGTERM");
 		}
+
+		LOGGER.setLevel(Configuration::getInstance().getLogLevel());
+		LOGGER.setFileName(Configuration::getInstance().getLogFileName());
 
 		Application::getInstance().init();
 		Application::getInstance().start();
